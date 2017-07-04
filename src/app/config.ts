d729a4e11cb0287e072b7960893830a2b98d8721
awmleer <awmleer@sparker.xyz>
2017-07-04 20:59:10 +0800

    [add] accountService init

---
+++ src/app/config.ts
@@ -0,0 +1,3 @@
+export const CONFIG = {
+  apiUrl:'http://123.206.177.174:3001'
+};
--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -1,19 +1,30 @@
 import {Injectable} from "@angular/core";
 import {Subject} from "rxjs/Subject";
-
+import {User} from "../classes/user";
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/toPromise'
+import {CONFIG} from "../app/config";
 
 @Injectable()
 export class AccountService {
-  gameModel=new Subject<any>();
-  gameModel$=this.gameModel.asObservable();
+  user:User;
 
 
-  constructor(){
-    this.gameModel.next({
-      a:1,
-      b:{
-        x:2,
-        y:3
+  constructor(
+    private http:Http
+  ){}
+
+  login(username:string, password: string):Promise<boolean>{
+    return this.http.get(CONFIG.apiUrl+`/account/login/`).toPromise().then(response=>{
+      let data = response.json();
+      if (data.status=='ok'){
+        this.user={
+          username:username,
+          id:data.payload.id
+        };
+        return true;
+      }else{
+        return false;
       }
     });
   }
