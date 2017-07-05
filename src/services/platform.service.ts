41003c4f458d6f01b9d4c45800488ca57a612c4d
awmleer <awmleer@sparker.xyz>
2017-07-05 20:12:11 +0800

    [add] platform service functions

--- src/services/platform.service.ts
+++ src/services/platform.service.ts
@@ -2,15 +2,40 @@ import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/toPromise'
 import {CONFIG} from "../app/config";
+import {ToastService} from "./toast.service";
 
 @Injectable()
 export class PlatformService {
   constructor(
-    private http: Http
+    private http: Http,
+    private toastSvc: ToastService
   ) {}
 
   getGameList():Promise<any>{
     return this.http.get(CONFIG.apiUrl+'/game/list/').toPromise();
+  }
+
+  getGameInfo(gameId:string):Promise<any>{
+    return this.http.get(CONFIG.apiUrl+`/game/${gameId}/info/`).toPromise();
+  }
+
+  getRoomList():Promise<any>{
+    return this.http.get(CONFIG.apiUrl+'/room/list/').toPromise();
+  }
+
+  getRoomInfo(roomId:string):Promise<any>{
+    return this.http.get(CONFIG.apiUrl+`/room/${roomId}/info/`).toPromise();
+  }
+
+  createRoom(gameId:string,roomName:string):Promise<any>{
+    return this.http.post(CONFIG.apiUrl+'/room/create/',{
+      gameTemplateId:gameId,
+      roomName:roomName
+    }).toPromise();
+  }
+
+  enterRoom(roomId:string):Promise<any>{
+    return this.http.get(CONFIG.apiUrl+`/room/${roomId}/enter/`).toPromise();
   }
 
 
