461a1ffa7e184347f6280a8940af4a16008eed91
awmleer <awmleer@sparker.xyz>
2017-07-05 20:16:33 +0800

    [add] class GameInfo

--- src/classes/game.ts
+++ src/classes/game.ts
@@ -3,3 +3,43 @@ export class GameBrief {
   name:string;
   picUri:string;
 }
+
+export class GameInfo {
+  _id: string;
+  name: string;
+  picUri: string;
+  stages: {
+    _id: number,
+    name: string,
+    description: string,
+    mode: string,
+    duration: number,
+    isVote: false,
+    canSurvey: true,
+    canSkill: true,
+    canItem: true,
+    canTrade: true
+  }[];
+  places: {
+    _id: number,
+    name: string,
+    description: string,
+    costAp: number
+  }[];
+  roles: {
+    _id: number,
+    name: string,
+    necessary: false,
+    picture: string,
+    avatar: string,
+    description: string,
+    costAp: number,
+    placesCanSurveyId: [
+      number
+      ]
+  }[];
+  playersNum: {
+    min: number,
+    max: number
+  };
+}
