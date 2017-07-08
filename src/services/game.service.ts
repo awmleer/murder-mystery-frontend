d87195aedfaf6bb3adb281df31338bd5c53b97dc
awmleer <awmleer@sparker.xyz>
2017-07-08 09:21:50 +0800

    [try] jsondiffpatch & model change event handle

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,20 +1,34 @@
 import {Injectable} from "@angular/core";
 import {Subject} from "rxjs/Subject";
 import {SocketService} from "./socket.service";
-
+import * as jdp from 'jsondiffpatch';
 
 @Injectable()
 export class GameService {
   roomModel;
   playerModel;//TODO type definition
+  private patcher;
 
   constructor(
     private socketSvc: SocketService
-  ){}
+  ){
+    this.patcher=jdp.create();
+    console.log(this.patcher);
+    this.socketSvc.on('changeRoomModel',(data)=>{
+      this.roomModel=this.patcher.patch(this.roomModel,data);
+      console.log(this.roomModel);
+    });
+    this.socketSvc.on('changeGameModel',(data)=>{
+      this.playerModel=this.patcher.patch(this.playerModel,data);
+      console.log(this.playerModel);
+    });
+  }
 
 
   initModel(callback:()=>any){
     this.socketSvc.call('initModel',(data)=>{
+      console.log('initModel got callback');
+      console.log(data);
       this.playerModel=data.initPlayer;
       this.roomModel=data.initRoom;
       callback();
@@ -26,5 +40,6 @@ export class GameService {
       roleId:roleId
     });
   }
+
 
 }
--- src/services/socket.service.ts
+++ src/services/socket.service.ts
@@ -32,15 +32,23 @@ export class SocketService {
   call(eventName:string, ...args:any[]){//call用来做双向的数据交互
     this.checkSocket();
     this.socket.emit(eventName,...args);
+    console.log(eventName + ' called');
   }
 
   inform(eventName:string, param?:object){//inform用来做单向的数据交互
+    this.checkSocket();
     if (!param) param={};
     this.socket.emit(eventName,param,(data)=>{
       if (data['status'] == 'fail') {
         this.toastSvc.toast(data['payload']);
       }
     });
+    console.log(eventName + ' informed');
+  }
+
+  on(eventName:string,callback:(data)=>void){
+    this.checkSocket();
+    this.socket.on(eventName, callback);
   }
 
 }
