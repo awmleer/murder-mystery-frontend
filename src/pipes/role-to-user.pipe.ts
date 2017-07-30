35e266de0dc62053078449612adff277615c7f72
awmleer <awmleer@sparker.xyz>
2017-07-30 18:22:18 +0800

    [fix] g form page

--- src/pipes/role-to-user.pipe.ts
+++ src/pipes/role-to-user.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import {roleId} from "../classes/model";
+import {GameService} from "../services/game.service";
 
 
 @Pipe({
@@ -7,8 +8,12 @@ import {roleId} from "../classes/model";
   pure: false
 })
 export class RoleToUserPipe implements PipeTransform {
-  transform(roleId: roleId, players) {
+  constructor(
+    private gameSvc: GameService
+  ){}
+  transform(roleId: roleId) {
   // transform(roleId: any) {
+    let players=this.gameSvc.roomModel.players;
     for (let userId in players) {
       if (players.hasOwnProperty(userId)) {
         if (players[userId].role.id == roleId) {
